aa596dbccb24ffba0de2b07f869a62bbc710ab0a
156789qq@gmail.com <Maiba.V@parmalogica.ru>
2019-03-13 17:19:01 +0200

    core 2.1

--- VSProdj/BookCoreApi/BookCoreApi/Startup.cs
+++ VSProdj/BookCoreApi/BookCoreApi/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+//using EFGetStarted.AspNetCore.NewDb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookCoreApi
 {
---
+++ VSProdj/BookCoreApi/DAL/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entity;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DAL
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Blogs { get; set; }
+        public DbSet<Book> Posts { get; set; }
+    }
+}
